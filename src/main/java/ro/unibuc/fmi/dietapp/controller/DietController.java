02dbb3af6c2b78b54e13f15c2da3c1989081b953
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-07 18:46:04 +0300

    Added Controllers for Diets

---
+++ src/main/java/ro/unibuc/fmi/dietapp/controller/DietController.java
@@ -0,0 +1,67 @@
+package ro.unibuc.fmi.dietapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.unibuc.fmi.dietapp.dto.DietDto;
+import ro.unibuc.fmi.dietapp.exception.BadRequestException;
+import ro.unibuc.fmi.dietapp.mapper.DietMapper;
+import ro.unibuc.fmi.dietapp.model.Diet;
+import ro.unibuc.fmi.dietapp.service.DietService;
+
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/diets")
+public class DietController {
+
+    private final DietService service;
+    private final DietMapper mapper;
+
+    public DietController(DietMapper mapper, DietService service) {
+        this.mapper = mapper;
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DietDto>> findAll(){
+        List<Diet> response = service.findAll();
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+   @GetMapping("/goal")
+    public ResponseEntity<List<DietDto>> findByGoal(@RequestParam Long id){
+        List<Diet> response = service.findByGoalId(id);
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @GetMapping("/type")
+    public ResponseEntity<List<DietDto>> findByType(@RequestParam Long id){
+        List<Diet> response = service.findByTypeId(id);
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DietDto> findById(@PathVariable Long id){
+        Diet response = service.findById(id);
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DietDto> update(@PathVariable Long id, @RequestBody DietDto request) {
+        if (!id.equals(request.getId())) {
+            throw new BadRequestException("The path variable doesn't match the request body id!");
+        }
+
+        Diet response = service.update(mapper.toEntity(request));
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
