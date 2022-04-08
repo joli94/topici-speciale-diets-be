4b067f95b50bec4d1f5f8a241bb010dc5967cb53
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-08 22:35:56 +0300

    Added Controllers for Foods

---
+++ src/main/java/ro/unibuc/fmi/dietapp/controller/FoodCategoryController.java
@@ -0,0 +1,37 @@
+package ro.unibuc.fmi.dietapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.unibuc.fmi.dietapp.dto.FoodCategoryDto;
+import ro.unibuc.fmi.dietapp.mapper.FoodCategoryMapper;
+import ro.unibuc.fmi.dietapp.model.FoodCategory;
+import ro.unibuc.fmi.dietapp.service.FoodCategoryService;
+
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/foodCategories")
+public class FoodCategoryController {
+    private final FoodCategoryService service;
+    private final FoodCategoryMapper mapper;
+
+    public FoodCategoryController(FoodCategoryService service, FoodCategoryMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FoodCategoryDto>> findAll(){
+        List<FoodCategory> response = service.findAll();
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodCategoryDto> findById(@PathVariable Long id) {
+        FoodCategory response = service.findById(id);
+        return new ResponseEntity<>(mapper.toDto(response), HttpStatus.OK);
+    }
+}
