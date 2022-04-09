783021f8861a1a2a0de8cea5be3b23ec9c1608ec
Einass-Albudkheel <einass.albudkheel@s.unibuc.ro>
2022-04-09 21:54:59 +0300

    Added Repo for City

---
+++ src/main/java/ro/unibuc/fmi/dietapp/repository/CityRepository.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.dietapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ro.unibuc.fmi.dietapp.model.City;
+
+
+import java.util.List;
+
+@Repository
+public interface CityRepository extends JpaRepository <City, Long> {
+    List<City> findByCountryId(Long id);
+}
