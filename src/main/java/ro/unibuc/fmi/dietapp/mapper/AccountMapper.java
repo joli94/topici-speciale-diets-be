5fb1a913552a93fe624ae3a19122d40086411c9a
Einass-Albudkheel <einass.albudkheel@s.unibuc.ro>
2022-04-09 22:49:10 +0300

    Added Mapper for Account

---
+++ src/main/java/ro/unibuc/fmi/dietapp/mapper/AccountMapper.java
@@ -0,0 +1,10 @@
+package ro.unibuc.fmi.dietapp.mapper;
+
+import org.mapstruct.Mapper;
+import ro.unibuc.fmi.dietapp.dto.AccountDto;
+import ro.unibuc.fmi.dietapp.model.Account;
+
+
+@Mapper
+public interface AccountMapper extends EntityMapper<AccountDto, Account>{
+}
