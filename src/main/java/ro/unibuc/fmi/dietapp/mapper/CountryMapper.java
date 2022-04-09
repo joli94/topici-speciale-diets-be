ee41e2d703f0fd41ebb639780fcfd6119cf4ab0c
Einass-Albudkheel <einass.albudkheel@s.unibuc.ro>
2022-04-09 12:50:56 +0300

    Added Mappers for Country

---
+++ src/main/java/ro/unibuc/fmi/dietapp/mapper/CountryMapper.java
@@ -0,0 +1,9 @@
+package ro.unibuc.fmi.dietapp.mapper;
+
+import org.mapstruct.Mapper;
+import ro.unibuc.fmi.dietapp.dto.CountryDto;
+import ro.unibuc.fmi.dietapp.model.Country;
+
+@Mapper
+public interface CountryMapper extends EntityMapper<CountryDto, Country>{
+}
