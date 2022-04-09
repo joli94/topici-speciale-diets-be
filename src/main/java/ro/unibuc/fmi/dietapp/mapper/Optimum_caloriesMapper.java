57659849b8a84f688c8b93dd0b895033a6346a04
Einass-Albudkheel <einass.albudkheel@s.unibuc.ro>
2022-04-10 00:21:16 +0300

    Added Mapper for Optimum-calories

---
+++ src/main/java/ro/unibuc/fmi/dietapp/mapper/Optimum_caloriesMapper.java
@@ -0,0 +1,10 @@
+package ro.unibuc.fmi.dietapp.mapper;
+
+import org.mapstruct.Mapper;
+import ro.unibuc.fmi.dietapp.dto.Optimum_caloriesDto;
+import ro.unibuc.fmi.dietapp.model.Optimum_calories;
+
+
+@Mapper
+public interface Optimum_caloriesMapper extends EntityMapper<Optimum_caloriesDto, Optimum_calories>{
+}
