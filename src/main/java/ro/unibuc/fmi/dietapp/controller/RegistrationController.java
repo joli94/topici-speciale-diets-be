982ac59c14c025b7e8070347692526976a598c55
Einass-Albudkheel <einass.albudkheel@s.unibuc.ro>
2022-04-09 23:18:34 +0300

    Added Controller for Registration

---
+++ src/main/java/ro/unibuc/fmi/dietapp/controller/RegistrationController.java
@@ -0,0 +1,28 @@
+package ro.unibuc.fmi.dietapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.unibuc.fmi.dietapp.dto.RegistrationDto;
+import ro.unibuc.fmi.dietapp.mapper.RegistrationMapper;
+import ro.unibuc.fmi.dietapp.service.RegistrationService;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+    private final RegistrationService service;
+    private final RegistrationMapper mapper;
+
+    public RegistrationController(RegistrationService service, RegistrationMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody RegistrationDto request){
+        service.create(mapper.toEntity(request));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
