0e84479ccae6a092d575cdeef6982ad7dd3c23b4
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-10 09:45:15 +0300

    Added test for FindAll

--- src/test/java/ro/unibuc/fmi/dietapp/service/IngredientServiceTest.java
+++ src/test/java/ro/unibuc/fmi/dietapp/service/IngredientServiceTest.java
@@ -1,6 +1,8 @@
 package ro.unibuc.fmi.dietapp.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -8,7 +10,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ro.unibuc.fmi.dietapp.model.Ingredient;
 import ro.unibuc.fmi.dietapp.repository.IngredientRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class IngredientServiceTest {
@@ -25,5 +32,28 @@ public class IngredientServiceTest {
 
     @BeforeEach
     void setUp() {
+        expected = Ingredient.builder()
+                .id(1L)
+                .name("afine")
+                .calories(61L)
+                .build();
+    }
+
+    @Test
+    @DisplayName("Find all ingredients - happy flow")
+    public void test_findAllIngredients_happyFlow() {
+        List<Ingredient> ingredientList = new ArrayList<>();
+        ingredientList.add(expected);
+
+        when(repository.findAll()).thenReturn(ingredientList);
+
+        List<Ingredient> result = service.findAll();
+
+        assertEquals(ingredientList.size(), result.size());
+        assertEquals(expected.getId(), result.stream().findFirst().get().getId());
+        assertEquals(expected.getName(), result.stream().findFirst().get().getName());
+        assertEquals(expected.getCalories(), result.stream().findFirst().get().getCalories());
+
+        verify(repository).findAll();
     }
 }
