6fe3d19e38596b091d9af8070e87b5a06a7b0d46
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-10 09:04:29 +0300

    Setup for testing

---
+++ src/main/java/ro/unibuc/fmi/dietapp/service/IngredientService.java
@@ -0,0 +1,7 @@
+package ro.unibuc.fmi.dietapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IngredientService {
+}
---
+++ src/test/java/ro/unibuc/fmi/dietapp/service/IngredientServiceTest.java
@@ -0,0 +1,15 @@
+package ro.unibuc.fmi.dietapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class IngredientServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
