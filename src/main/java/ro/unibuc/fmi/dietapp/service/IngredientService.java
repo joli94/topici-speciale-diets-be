b21aed20458d8fe6c37b4e9add8b56d12dee149d
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-10 09:08:06 +0300

    Added required components

--- src/main/java/ro/unibuc/fmi/dietapp/service/IngredientService.java
+++ src/main/java/ro/unibuc/fmi/dietapp/service/IngredientService.java
@@ -1,7 +1,15 @@
 package ro.unibuc.fmi.dietapp.service;
 
 import org.springframework.stereotype.Service;
+import ro.unibuc.fmi.dietapp.repository.IngredientRepository;
 
 @Service
 public class IngredientService {
+    private final IngredientRepository ingredientRepository;
+    private final FoodIngredientsService foodIngredientsService;
+
+    public IngredientService(IngredientRepository ingredientRepository, FoodIngredientsService foodIngredientsService) {
+        this.ingredientRepository = ingredientRepository;
+        this.foodIngredientsService = foodIngredientsService;
+    }
 }
--- src/test/java/ro/unibuc/fmi/dietapp/service/IngredientServiceTest.java
+++ src/test/java/ro/unibuc/fmi/dietapp/service/IngredientServiceTest.java
@@ -2,12 +2,26 @@ package ro.unibuc.fmi.dietapp.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ro.unibuc.fmi.dietapp.model.Ingredient;
+import ro.unibuc.fmi.dietapp.repository.IngredientRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class IngredientServiceTest {
+    @Mock
+    private IngredientRepository repository;
+
+    @Mock
+    private FoodIngredientsService foodIngredientsService;
+
+    @InjectMocks
+    private IngredientService service;
+
+    private Ingredient expected;
 
     @BeforeEach
     void setUp() {
