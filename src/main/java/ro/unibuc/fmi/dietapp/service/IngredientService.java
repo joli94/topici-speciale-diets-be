69adc70ad04874357ca2669bcddff2cf80854a47
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-10 10:28:54 +0300

    Corrected bug for creating constructors in FoodIngredients

--- src/main/java/ro/unibuc/fmi/dietapp/service/IngredientService.java
+++ src/main/java/ro/unibuc/fmi/dietapp/service/IngredientService.java
@@ -2,9 +2,11 @@ package ro.unibuc.fmi.dietapp.service;
 
 import org.springframework.stereotype.Service;
 import ro.unibuc.fmi.dietapp.exception.EntityNotFoundException;
+import ro.unibuc.fmi.dietapp.model.FoodIngredients;
 import ro.unibuc.fmi.dietapp.model.Ingredient;
 import ro.unibuc.fmi.dietapp.repository.IngredientRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +21,15 @@ public class IngredientService {
 
     public List<Ingredient> findAll() {
         return ingredientRepository.findAll();
+    }
+
+    public List<Ingredient> findByFoodId(Long id){
+        List<Ingredient> ingredientList = new ArrayList<>();
+        List<FoodIngredients> result =  foodIngredientsService.findByFoodId(id);
+
+        result.forEach((foodIngredients) -> ingredientList.add(findById(foodIngredients.getId())));
+
+        return ingredientList;
     }
 
     public Ingredient findById(Long id){
