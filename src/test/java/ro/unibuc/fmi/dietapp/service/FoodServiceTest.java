4547123ed1bf59c2eb7be540dda6b1b1867d9634
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-11 10:32:03 +0300

    Added test_findFoodById_happyFlow

--- src/test/java/ro/unibuc/fmi/dietapp/service/FoodServiceTest.java
+++ src/test/java/ro/unibuc/fmi/dietapp/service/FoodServiceTest.java
@@ -1,5 +1,6 @@
 package ro.unibuc.fmi.dietapp.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ro.unibuc.fmi.dietapp.exception.EntityNotFoundException;
 import ro.unibuc.fmi.dietapp.model.Food;
 import ro.unibuc.fmi.dietapp.model.FoodCategory;
 import ro.unibuc.fmi.dietapp.model.Ingredient;
@@ -14,7 +16,9 @@ import ro.unibuc.fmi.dietapp.repository.FoodRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -59,6 +63,23 @@ class FoodServiceTest {
         assertEquals(expected.getFoodCategory(), result.stream().findFirst().get().getFoodCategory());
 
         verify(repository).findAll();
+    }
+
+    @Test
+    @DisplayName("Find a food by id - happy flow")
+    public void test_findFoodById_happyFlow() {
+        Long id = expected.getId();
+
+        when(repository.findById(id)).thenReturn(Optional.of(expected));
+
+        Food result = service.findById(id);
+
+        assertEquals(expected.getId(), result.getId());
+        assertEquals(expected.getName(), result.getName());
+        assertEquals(expected.getCalories(), result.getCalories());
+        assertEquals(expected.getFoodCategory(), result.getFoodCategory());
+
+        verify(repository).findById(id);
     }
 
 
