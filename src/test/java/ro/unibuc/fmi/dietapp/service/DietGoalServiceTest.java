37a8f6b04b291798a90507dae66018c05872f15e
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-11 11:54:44 +0300

    Added test_findAllDietGoals_happyFlow

---
+++ src/test/java/ro/unibuc/fmi/dietapp/service/DietGoalServiceTest.java
@@ -0,0 +1,53 @@
+package ro.unibuc.fmi.dietapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ro.unibuc.fmi.dietapp.model.DietGoal;
+import ro.unibuc.fmi.dietapp.repository.DietGoalRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DietGoalServiceTest {
+    @Mock
+    private DietGoalRepository repository;
+
+    @InjectMocks
+    private DietGoalService service;
+
+    private DietGoal expected;
+
+    @BeforeEach
+    void setUp() {
+        expected = DietGoal.builder()
+                .id(1L)
+                .name("slabire")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Find all diet goals - happy flow")
+    public void test_findAllDietGoals_happyFlow() {
+        Long id = expected.getId();
+
+        List<DietGoal> expectedList = new ArrayList<>();
+        expectedList.add(expected);
+
+        when(repository.findAll()).thenReturn(expectedList);
+
+        List<DietGoal> result = service.findAll();
+
+        assertEquals(expectedList.size(), result.size());
+        assertEquals(expected.getId(), result.stream().findFirst().get().getId());
+        assertEquals(expected.getName(), result.stream().findFirst().get().getName());
+    }
+}
