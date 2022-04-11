4d0bd448f95e7997250d58085608d0da9c942601
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-11 11:02:12 +0300

    Added test_findFoodByDietId_happyFlow

--- src/test/java/ro/unibuc/fmi/dietapp/service/FoodServiceTest.java
+++ src/test/java/ro/unibuc/fmi/dietapp/service/FoodServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ro.unibuc.fmi.dietapp.exception.EntityNotFoundException;
+import ro.unibuc.fmi.dietapp.model.DietPlan;
 import ro.unibuc.fmi.dietapp.model.Food;
 import ro.unibuc.fmi.dietapp.model.FoodCategory;
 import ro.unibuc.fmi.dietapp.model.Ingredient;
@@ -21,11 +22,12 @@ import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.endsWith;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class FoodServiceTest {
+public class FoodServiceTest {
     @Mock
     private FoodRepository repository;
 
@@ -36,6 +38,7 @@ class FoodServiceTest {
     private FoodService service;
 
     private Food expected;
+    private List<DietPlan> expectedDietPlanList;
 
     @BeforeEach
     void setUp() {
@@ -45,6 +48,13 @@ class FoodServiceTest {
                 .calories(150L)
                 .foodCategory(FoodCategory.builder().id(1L).name("lunch").build())
                 .build();
+
+        DietPlan expectedDietPlan = DietPlan.builder().id(1L).build();
+
+        expectedDietPlanList = new ArrayList<>();
+        expectedDietPlanList.add(expectedDietPlan);
+
+        expected.setDietPlanList(expectedDietPlanList);
     }
 
     @Test
@@ -62,6 +72,7 @@ class FoodServiceTest {
         assertEquals(expected.getName(), result.stream().findFirst().get().getName());
         assertEquals(expected.getCalories(), result.stream().findFirst().get().getCalories());
         assertEquals(expected.getFoodCategory(), result.stream().findFirst().get().getFoodCategory());
+        assertEquals(expected.getDietPlanList(), result.stream().findFirst().get().getDietPlanList());
 
         verify(repository).findAll();
     }
@@ -79,6 +90,7 @@ class FoodServiceTest {
         assertEquals(expected.getName(), result.getName());
         assertEquals(expected.getCalories(), result.getCalories());
         assertEquals(expected.getFoodCategory(), result.getFoodCategory());
+        assertEquals(expected.getDietPlanList(), result.getDietPlanList());
 
         verify(repository).findById(id);
     }
@@ -97,5 +109,51 @@ class FoodServiceTest {
         assertThat(exception.getMessage()).isEqualTo("The food with this id doesn't exist in the database!");
 
         verify(repository).findById(id);
+    }
+
+    @Test
+    @DisplayName("Find a food by food category id - happy flow")
+    public void test_findFoodByFoodCategoryId_happyFlow() {
+        Long id = expected.getFoodCategory().getId();
+
+        List<Food> foodList = new ArrayList<>();
+        foodList.add(expected);
+
+        when(repository.findByFoodCategoryId(id)).thenReturn(foodList);
+
+        List<Food> result = service.findByFoodCategory(id);
+
+        assertEquals(foodList.size(), result.size());
+        assertEquals(expected.getId(), result.stream().findFirst().get().getId());
+        assertEquals(expected.getName(), result.stream().findFirst().get().getName());
+        assertEquals(expected.getCalories(), result.stream().findFirst().get().getCalories());
+        assertEquals(expected.getFoodCategory(), result.stream().findFirst().get().getFoodCategory());
+        assertEquals(expected.getDietPlanList(), result.stream().findFirst().get().getDietPlanList());
+
+        verify(repository).findByFoodCategoryId(id);
+    }
+
+    @Test
+    @DisplayName("Find a food by diet id - happy flow")
+    public void test_findFoodByDietId_happyFlow() {
+        Long id = expected.getDietPlanList().stream().findFirst().get().getId();
+        Long foodId = expected.getId();
+
+        List<Food> foodList = new ArrayList<>();
+        foodList.add(expected);
+
+        when(dietPlanService.findByDiet(id)).thenReturn(expectedDietPlanList);
+        when(repository.findById(foodId)).thenReturn(Optional.of(expected));
+
+        List<Food> result = service.findByDiet(id);
+
+        assertEquals(foodList.size(), result.size());
+        assertEquals(expected.getId(), result.stream().findFirst().get().getId());
+        assertEquals(expected.getName(), result.stream().findFirst().get().getName());
+        assertEquals(expected.getCalories(), result.stream().findFirst().get().getCalories());
+        assertEquals(expected.getFoodCategory(), result.stream().findFirst().get().getFoodCategory());
+        assertEquals(expected.getDietPlanList(), result.stream().findFirst().get().getDietPlanList());
+
+        verify(dietPlanService).findByDiet(id);
     }
 }
