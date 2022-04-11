3f6075f150355d17927d5bfce3cf24ad387c44d3
joli94 <2558817+joli94@users.noreply.github.com>
2022-04-11 11:56:31 +0300

    Added test_findDietGoalById_happyFlow

--- src/test/java/ro/unibuc/fmi/dietapp/service/DietGoalServiceTest.java
+++ src/test/java/ro/unibuc/fmi/dietapp/service/DietGoalServiceTest.java
@@ -8,12 +8,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ro.unibuc.fmi.dietapp.model.DietGoal;
+import ro.unibuc.fmi.dietapp.model.Ingredient;
 import ro.unibuc.fmi.dietapp.repository.DietGoalRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +52,22 @@ public class DietGoalServiceTest {
         assertEquals(expectedList.size(), result.size());
         assertEquals(expected.getId(), result.stream().findFirst().get().getId());
         assertEquals(expected.getName(), result.stream().findFirst().get().getName());
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    @DisplayName("Find a diet goal by id - happy flow")
+    public void test_findDietGoalById_happyFlow() {
+        Long id = expected.getId();
+
+        when(repository.findById(id)).thenReturn(Optional.of(expected));
+
+        DietGoal result = service.findById(id);
+
+        assertEquals(expected.getId(), result.getId());
+        assertEquals(expected.getName(), result.getName());
+
+        verify(repository).findById(id);
     }
 }
